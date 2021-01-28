c2edd3630a5017b709f22076b5b7732dad38eb51
hungl3-99 <61909619+hungl3-99@users.noreply.github.com>
2021-01-28 23:55:18 +0700

    p2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 import PageWrapper from './components/PageWrapper'
+import Home from './components/Pages/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import About from './components/Pages/About';
 
 function App() {
   return (
-    <PageWrapper />
+    <Router>
+      <PageWrapper >
+
+        <Route path='/'  exact component={Home} />
+        <Route path='/about' component ={About} />
+      </PageWrapper>
+    </Router>
+
+
   );
 }
 
